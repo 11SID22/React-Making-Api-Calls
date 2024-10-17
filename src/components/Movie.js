d97f700be656c15d3e5be59b0abaaefff54101ea
11SID22 <sid111097@gmail.com>
2024-10-17 18:40:17 +0530

    Added a delete button to delete the movies from the database and UI.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
   return (
     <li className={classes.movie}>
-      <h2>{props.title}</h2>
-      <h3>{props.releaseDate}</h3>
-      <p>{props.openingText}</p>
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.openingText}</p>
+        <p>{props.releaseDate}</p>
+        {/* Delete Button */}
+        <button onClick={() => props.onDeleteMovie(props.id)}>Delete</button>
+      </div>
     </li>
   );
 };
